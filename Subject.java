f88920d0271cb25e71cf78b7cf2fd76898373d17
SuperMman2017 <48609065+SuperMman2017@users.noreply.github.com>
2020-04-27 23:57:59 +0800

    game menu, Battle system

--- Subject.java
+++ Subject.java
@@ -2,5 +2,5 @@
 public interface Subject {
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
-    public void notifyObservers();
+    public void notifyObsgervers();
 }
