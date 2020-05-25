1846b9ba8d3ad999748f20f8c4ff8c939808cc32
SuperMman2017 <48609065+SuperMman2017@users.noreply.github.com>
2020-05-25 13:39:33 +0800

    fixed decorator pattern

--- SpecialAbility.java
+++ SpecialAbility.java
@@ -1,6 +1,20 @@
+public abstract class SpecialAbility extends Enemy {
+    protected Enemy enemy;
 
-public interface SpecialAbility {    
-    
-    public abstract void specialAbility();
+    public SpecialAbility(Enemy enemy) {
+        super(enemy.getName(), enemy.getMaxHealth());
+        this.enemy = enemy;
+    }
 
+    @Override public void attack() {
+        enemy.attack();
+    }
+
+    @Override public void setHealth(int health) {
+        enemy.setHealth(health);
+    }
+
+    @Override public void setAttack(int attack) {
+        enemy.setAttack(attack);
+    }
 }
