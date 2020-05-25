8c63ce2ebbf6721b44f9d2cf36a7d531df01cc94
Oli Ramilo <lolh2017@gmail.com>
2020-05-25 17:47:00 +0800

    finish bag class and move on the game ui for battle system and menu system

---
+++ PlayerBag.java
@@ -0,0 +1,45 @@
+/*  Player bag class that stores in-game Item objects 
+    for player to keep over the course of the game*/
+public class PlayerBag {
+
+    public static final int EMPTY = 0;
+    private LinkedList<Item> bag;
+    private int maxSize;
+    private int currentSize;
+
+    public PlayerBag(int maxSize) {
+        bag = new LinkedList<>();
+        this.maxSize = maxSize;
+        currentSize = 0;
+    }
+
+    /*Adds Item object to the Item bag*/
+    public boolean addItem(Item item) {
+        if(maxSize > currentSize) {
+            bag.add(item);
+            return true;
+        }
+        return false;
+    }
+
+    /*Returns the item matching the name parameter*/
+    public Item getItem(String name) {
+        for(Item i: bag) {
+            if(i.getName().equals(name)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    /*Removes an item from the bag*/
+    public boolean removeItem(String name) {
+        for(Item i: bag) {
+            if(i.getName().equals(name)) {
+                bag.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
