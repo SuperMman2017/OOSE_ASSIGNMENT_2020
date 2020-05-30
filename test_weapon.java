7757fbc473494f587ba31b624deefd0e7605389b
SuperMman2017 <48609065+SuperMman2017@users.noreply.github.com>
2020-05-30 18:06:59 +0800

    sdfsfsdfsdfsdfds

--- test_weapon.java
+++ test_weapon.java
@@ -1,9 +1,9 @@
 public class test_weapon {
     public static void main(String [] args) {
-        Weapon weapon = new Weapon("Stick", 10);
+        Weapon weapon = new WeaponDecorator("Stick", 10);
         weapon.setStat("Hurt", "Wood", 5, 10);
         weapon.setDescription(". Poke your enemies with this stick for weak damage.");
-        EnchantmentDamage decoratedWeapon = new EnchantmentDamage(weapon);
+        WeaponDecorator decoratedWeapon = new EnchantmentDamage(weapon);
         decoratedWeapon.setEnchantment(10);
         System.out.println(decoratedWeapon.getName() + decoratedWeapon.getDescription());
     }
